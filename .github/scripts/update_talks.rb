499bb4dfb952ad3ddf79e38f99b097936d63aea2
Ben Greenberg <13892277+bencgreenberg@users.noreply.github.com>
2023-04-05 15:01:56 +0300

    Update update_talks.rb

--- .github/scripts/update_talks.rb
+++ .github/scripts/update_talks.rb
@@ -9,7 +9,7 @@ parsed_page = Nokogiri::HTML(response.body)
 talks = parsed_page.css('tbody tr')
 
 # Generate the updated talks list (top 5)
-talks_list = ["\nSome recent talks I've given at conferences include:\n"]
+talks_list = ["Some recent talks I've given at conferences include:\n"]
 talks.first(5).each do |talk|
   presentation = talk.css('div.text-sm.font-medium.text-gray-900').text.strip
   conference = talk.css('td:nth-child(2) div.text-sm.text-gray-900').text.strip
@@ -23,8 +23,8 @@ repo = ENV['GITHUB_REPOSITORY']
 readme = client.readme(repo)
 readme_content = Base64.decode64(readme[:content]).force_encoding('UTF-8')
 
-# Remove the existing talks list if present, otherwise, replace the placeholder
-talks_regex = /(?<=\nSome recent talks I've given at conferences include:\n)([\s\S]*?)(?=\n\n)/m
-updated_content = readme_content.match(talks_regex) ? readme_content.sub(talks_regex, talks_list[1..].join("\n")) : readme_content.sub("<!-- recent_talks_placeholder -->", talks_list.join("\n"))
+# Remove the existing talks list if present
+talks_regex = /Some recent talks I've given at conferences include:[\s\S]*?(?=I've also had a chance to chat with some great people on podcasts including:)/m
+updated_content = readme_content.sub(talks_regex, talks_list.join("\n"))
 
 client.update_contents(repo, 'README.md', 'Update recent talks', readme[:sha], updated_content)
