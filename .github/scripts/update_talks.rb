1d2cb6a1dea25f69ac8a99040bfb6583c210f945
Ben Greenberg <13892277+bencgreenberg@users.noreply.github.com>
2023-04-05 14:46:04 +0300

    Update script to prevent duplication

--- .github/scripts/update_talks.rb
+++ .github/scripts/update_talks.rb
@@ -9,7 +9,7 @@ parsed_page = Nokogiri::HTML(response.body)
 talks = parsed_page.css('tbody tr')
 
 # Generate the updated talks list (top 5)
-talks_list = ["Some recent talks I've given at conferences include:\n"]
+talks_list = ["\nSome recent talks I've given at conferences include:\n"]
 talks.first(5).each do |talk|
   presentation = talk.css('div.text-sm.font-medium.text-gray-900').text.strip
   conference = talk.css('td:nth-child(2) div.text-sm.text-gray-900').text.strip
@@ -23,6 +23,6 @@ repo = ENV['GITHUB_REPOSITORY']
 readme = client.readme(repo)
 readme_content = Base64.decode64(readme[:content]).force_encoding('UTF-8')
 
-updated_content = readme_content.sub(/(?<=Some recent talks I've given at conferences include:)[\s\S]*?(?=\n\n)/m, "\n" + talks_list.join("\n"))
+updated_content = readme_content.sub(/(?<=Some recent talks I've given at conferences include:)([\s\S]*?)(?=\n\n)/m, talks_list.join("\n"))
 
 client.update_contents(repo, 'README.md', 'Update recent talks', readme[:sha], updated_content)
