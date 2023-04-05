707f8fefaaea48c94ea7f30f1d26d6b14792910d
Ben Greenberg <13892277+bencgreenberg@users.noreply.github.com>
2023-04-05 14:52:26 +0300

    3rd time is the charm
    Replace existing talks section w/new content w/o duplication

--- .github/scripts/update_talks.rb
+++ .github/scripts/update_talks.rb
@@ -23,6 +23,8 @@ repo = ENV['GITHUB_REPOSITORY']
 readme = client.readme(repo)
 readme_content = Base64.decode64(readme[:content]).force_encoding('UTF-8')
 
-updated_content = readme_content.sub(/(?<=Some recent talks I've given at conferences include:)([\s\S]*?)(?=\n\n)/m, talks_list.join("\n"))
+# Remove the existing talks list if present, otherwise, replace the placeholder
+talks_regex = /(?:Some recent talks I've given at conferences include:)([\s\S]*?)(?=\n\n)/m
+updated_content = readme_content.match(talks_regex) ? readme_content.sub(talks_regex, talks_list.join("\n")) : readme_content.sub("<!-- recent_talks_placeholder -->", talks_list.join("\n"))
 
 client.update_contents(repo, 'README.md', 'Update recent talks', readme[:sha], updated_content)
