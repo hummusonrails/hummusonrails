861865b602dd728143024e2eb9ac72f28f51f509
Ben Greenberg <13892277+bencgreenberg@users.noreply.github.com>
2023-04-05 15:22:02 +0300

    Dont duplicate blog posts section

--- .github/scripts/update_posts.rb
+++ .github/scripts/update_posts.rb
@@ -22,8 +22,8 @@ repo = ENV['GITHUB_REPOSITORY']
 readme = client.readme(repo)
 readme_content = Base64.decode64(readme[:content]).force_encoding('UTF-8')
 
-# Insert the blog posts list after the picture
-picture_regex = /(<img src="https:\/\/www.bengreenberg.dev\/assets\/images\/ben_and_matz.jpeg" align="center" style="width: 100%" \/>\n<\/div>)/
-updated_content = readme_content.sub(picture_regex, "\\1\n\n#{posts_list.join("\n")}\n")
+# Replace the existing blog posts section
+posts_regex = /### Recent Blog Posts\n\n[\s\S]*?(?=<\/td>)/m
+updated_content = readme_content.sub(posts_regex, "#{posts_list.join("\n")}\n")
 
 client.update_contents(repo, 'README.md', 'Update recent blog posts', readme[:sha], updated_content)
